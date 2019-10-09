0745d335ebdc487f0f9dc1d76e818699e039cbb2
diyat <diyat@172.20.10.6>
2019-10-09 21:12:58 +0700

    change run configuration on Netbeans 8.2

--- index.php
+++ index.php
@@ -1,21 +1,7 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="windows-1252">
-        <title></title>
-    </head>
-    <body>
-        <?php
-            $myPartyAges = array(20, 21, 22, 23, 24);
-            
-            for ($i = 0; $i < count($myPartyAges); $i++) {
-                echo $myPartyAges[$i] . '<br>';
-            }
-        ?>
-    </body>
-</html>
+<?php
+
+$myPartyAges = [20, 21, 22, 23, 24];
+
+for ($i = 0; $i < count($myPartyAges); $i++) {
+    echo $myPartyAges[$i] . "\n";
+}
